b2f1208087317cb98dc6817ccc1f2a15a68c3f96
draco <draco25vs@gmail.com>
2025-01-13 13:29:26 +0100

    Quitando un 'echo' conflictivo

--- instalacion.sh
+++ instalacion.sh
@@ -107,7 +107,6 @@ arch-chroot /mnt bash -c "
   systemctl enable NetworkManager
 
   # Instalación del gestor de arranque
-  echo "Instalando el gestor de arranque..."
   grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
   grub-mkconfig -o /boot/grub/grub.cfg
 "
